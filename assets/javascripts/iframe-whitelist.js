95f4f5aebf53694f76c1e865fdd876f67ae30867
SimplyLab <support@simplylab.com>
2017-03-27 11:39:23 +0200

    Update iframe-whitelist.js

--- assets/javascripts/iframe-whitelist.js
+++ assets/javascripts/iframe-whitelist.js
@@ -1,6 +1,6 @@
 (function() {
   if (Discourse.dialect_deprecated) { return; }
 
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.surveygizmo\.com\/.+/i);
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/w\.soundcloud\.com\/player\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/simplylab\.io\/.+/i);
+
 })();
